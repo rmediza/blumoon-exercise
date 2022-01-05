b8137aed0b754b4abca8b8e2cd0a60a272e87be5
rmediza <96425073+rmediza@users.noreply.github.com>
2022-01-05 20:23:46 +0800

    Update shelvesTB.sql

--- shelvesTB.sql
+++ shelvesTB.sql
@@ -1,19 +1,24 @@
 CREATE TABLE shelf_TB(
     shelfID INT GENERATED ALWAYS AS IDENTITY,
     category       TEXT   NOT NULL,
-    location       TEXT   NOT NULL
+    location       TEXT   NOT NULL,
+    PRIMARY KEY(shelfID)
 );
 
 CREATE TABLE level_TB(
     levelID INT GENERATED ALWAYS AS IDENTITY,
-    bookID         INT    NOT NULL,
-    shelfID        INT    NOT NULL
+    shelfID        INT    NOT NULL,
+    PRIMARY KEY(levelID),
+    CONSTRAINT fk_level
+      FOREIGN KEY(levelID) 
+	  REFERENCES levels(levelID)
 );
 
 CREATE TABLE shelves_TB(
     shelvesID INT GENERATED ALWAYS AS IDENTITY,
     levelID        INT    NOT NULL,
     dateCreated    DATE   NOT NULL,
+    PRIMARY KEY(levelID),
     CONSTRAINT fk_level
       FOREIGN KEY(levelID) 
 	  REFERENCES levels(levelID)
