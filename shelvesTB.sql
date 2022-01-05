bbdb1fba54fea8642fca63cdc42a22b00a7fd4cb
rmediza <96425073+rmediza@users.noreply.github.com>
2022-01-05 14:31:33 +0800

    Create shelvesTB.sql
    shelves tables sql script

---
+++ shelvesTB.sql
@@ -0,0 +1,16 @@
+CREATE TABLE shelf_TB(
+    shelfID SERIAL PRIMARY KEY NOT NULL,
+    category       TEXT   NOT NULL,
+    location       TEXT   NOT NULL
+);
+
+CREATE TABLE level_TB(
+    levelID SERIAL PRIMARY KEY NOT NULL,
+    bookID          INT    NOT NULL
+);
+
+CREATE TABLE shelves_TB(
+    shelvesID SERIAL PRIMARY KEY NOT NULL,
+    levelID          INT    NOT NULL,
+    dateCreated      DATE   NOT NULL
+);
