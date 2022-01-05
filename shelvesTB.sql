064dd7f2e814f09da142a9a5a000f49a60433083
rmediza <96425073+rmediza@users.noreply.github.com>
2022-01-05 17:57:03 +0800

    Update shelvesTB.sql

--- shelvesTB.sql
+++ shelvesTB.sql
@@ -7,10 +7,14 @@ CREATE TABLE shelf_TB(
 CREATE TABLE level_TB(
     levelID SERIAL PRIMARY KEY NOT NULL,
     bookID          INT    NOT NULL
+    
 );
 
 CREATE TABLE shelves_TB(
     shelvesID SERIAL PRIMARY KEY NOT NULL,
     levelID          INT    NOT NULL,
-    dateCreated      DATE   NOT NULL
+    dateCreated      DATE   NOT NULL,
+    CONSTRAINT fk_levels
+      FOREIGN KEY(levelID) 
+	  REFERENCES levels(levelID)
 );
