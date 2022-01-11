1d27c6b4cdb8b185fdfbc4b66e3ecbe2dfa58c13
rmediza <rediza.k11501159@umak.edu.ph>
2022-01-11 17:17:38 +0800

    Update 20220111090003_create_authors_TB_table.js

--- migrations/20220111090003_create_authors_TB_table.js
+++ migrations/20220111090003_create_authors_TB_table.js
@@ -1,5 +1,5 @@
 
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema.createTable('authorsTB', function (table) {
     table.increments('authorID').primary();
     table.text('firstName').notNullable();
@@ -11,6 +11,6 @@ exports.up = function(knex) {
   });
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTableIfExists('authorsTB');
 };
