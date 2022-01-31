e6ff04eb2acc629cd6146ffcfbef1fb278d4a0bc
rmediza <96425073+rmediza@users.noreply.github.com>
2022-01-31 11:39:04 +0800

    Update migration_authorsTB.test.js

--- migration_authorsTB.test.js
+++ migration_authorsTB.test.js
@@ -1,4 +1,4 @@
-const { test } = require('tap');
+const { test, end } = require('tap');
 const knex = require('knex')({
   client: 'pg',
   connection: {
@@ -10,22 +10,15 @@ const knex = require('knex')({
   }
 });
 
-const testTables = function (knex, table) {
-  return knex.schema.hasTable(table).then((result) => {
-    return (result) ? true : false;
+const testTables = function (knex) {
+  return knex.schema.hasTable('authorsTB');
   });
 };
 
-async function tst() {
-  let y = await testTables(knex, 'authorsTB');
-  let z = await testTables(knex, 'TBauthors');
-
 // to check, result, message
-  test('Checking if Table exist', ({ equal, end }) => {
-    equal(y, true, 'authorsTB must ok for expected result is true');
-    equal(z, false, 'TBauthors must ok for expected result is false');
-    end();
-  });
-}
+test('Checking if Table exist', ({ equal }) => {
+  let y = await testTables(knex);
+  equal(y, true, 'authorsTB must ok for expected result is true');
+});
 
-tst();
+end();
